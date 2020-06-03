4a102b2dacd34fbb74354c5c5e9beb93f5d47d11
Ardi Tri Heru <37565758+arditriheru@users.noreply.github.com>
2020-06-03 15:42:08 +0700

    Add files via upload

---
+++ views/footer.php
@@ -0,0 +1,18 @@
+<!-- footer -->
+<script>
+      // Loading Page
+      var myVar;
+      function myFunction() {
+      myVar = setTimeout(showPage, 500);
+      }
+      function showPage() {
+      document.getElementById("loader").style.display = "none";
+      document.getElementById("myDiv").style.display = "block";
+      }
+    </script>
+<script src="vendors/jquery/dist/jquery.min.js"></script>
+    <script src="vendors/bootstrap/dist/js/bootstrap.min.js"></script>
+    <script src="assets/js/main.js"></script>
+    <script src="assets/js/dashboard.js"></script>
+</body>
+</html>
