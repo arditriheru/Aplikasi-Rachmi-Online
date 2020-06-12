f04226cb7ab040fac56d1e5ab364ffbb8c8739cb
Ardi Tri Heru <37565758+arditriheru@users.noreply.github.com>
2020-06-12 15:16:20 +0700

    Add files via upload

--- index.php
+++ index.php
@@ -1,81 +1,81 @@
 <?php include "views/header.php"; ?>
-    <div class="content mt-3">
-        <div class="row">
+<div class="content mt-3">
+    <div class="row">
         <div class="col">
             <section class="card">
-            <a href="registration">
-                <div class="social-box color-1">
-                <i class="fa fa-stethoscope"></i>
-                <ul>
-                    <h5 class="blacktext">Pendaftaran</h5>
-                </ul>
-                </div>
-            </a><!--/social-box-->
+                <a href="registration">
+                    <div class="social-box color-1">
+                        <i class="fa fa-stethoscope"></i>
+                        <ul>
+                            <h5 class="blacktext">Pendaftaran</h5>
+                        </ul>
+                    </div>
+                </a><!--/social-box-->
             </section>
         </div>
         <div class="col">
             <section class="card">
-            <a href="search-schedule">
-                <div class="social-box color-2">
-                <i class="fa fa-calendar"></i>
-                <ul>
-                    <h5 class="blacktext">Jadwal</h5>
-                </ul>
-                </div>
-            </a><!--/social-box-->
+                <a href="search-schedule">
+                    <div class="social-box color-2">
+                        <i class="fa fa-calendar"></i>
+                        <ul>
+                            <h5 class="blacktext">Jadwal</h5>
+                        </ul>
+                    </div>
+                </a><!--/social-box-->
             </section>
         </div>
-        </div>
-        <div class="row">
-            <div class="col">
-                <section class="card">
+    </div>
+    <div class="row">
+        <div class="col">
+            <section class="card">
                 <a href="show-room">
                     <div class="social-box color-5">
-                    <i class="fa fa-bed"></i>
-                    <ul>
-                        <h5 class="blacktext">Bed</h5>
-                    </ul>
+                        <i class="fa fa-bed"></i>
+                        <ul>
+                            <h5 class="blacktext">Bed</h5>
+                        </ul>
                     </div>
                 </a><!--/social-box-->
-                </section>
-            </div>
-            <div class="col">
-                <section class="card">
+            </section>
+        </div>
+        <div class="col">
+            <section class="card">
                 <a href="show-queue">
                     <div class="social-box color-4">
-                    <i class="fa fa-group"></i>
-                    <ul>
-                        <h5 class="blacktext">Antrian</h5>
-                    </ul>
+                        <i class="fa fa-group"></i>
+                        <ul>
+                            <h5 class="blacktext">Antrian</h5>
+                        </ul>
                     </div>
                 </a><!--/social-box-->
-                </section>
-            </div>
-            </div>
-            <div class="row">
-            <div class="col">
-                <section class="card">
+            </section>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col">
+            <section class="card">
                 <a href="price-list">
                     <div class="social-box color-3">
-                    <i class="fa fa-list"></i>
-                    <ul>
-                        <h5 class="blacktext">Harga</h5>
-                    </ul>
+                        <i class="fa fa-list"></i>
+                        <ul>
+                            <h5 class="blacktext">Harga</h5>
+                        </ul>
                     </div>
                 </a><!--/social-box-->
-                </section>
-            </div>
-            <div class="col">
-                <section class="card">
-                <a href="#">
+            </section>
+        </div>
+        <div class="col">
+            <section class="card">
+                <a href="information">
                     <div class="social-box color-a">
-                    <i class="fa fa-bullhorn"></i>
-                    <ul>
-                        <h5 class="blacktext">Info</h5>
-                    </ul>
+                        <i class="fa fa-bullhorn"></i>
+                        <ul>
+                            <h5 class="blacktext">Info</h5>
+                        </ul>
                     </div>
                 </a><!--/social-box-->
-                </section>
-            </div>
-            </div>
-<?php include "views/footer.php"; ?> 
+            </section>
+        </div>
+    </div>
+    <?php include "views/footer.php"; ?> 
---
+++ information.php
@@ -0,0 +1,26 @@
+<?php include "views/header.php"; ?>
+<div class="content mt-3">
+    <div class="animated fadeIn">
+        <div class="row">
+            <div class="col-lg-12">
+                <div class="row">
+                    <div class="col-lg-6">
+                        <div align="center"><img class="img-responsive" src="images/information1.jpg" width="50%" alt="Koneksi internet lambat.."></div>
+                    </div>
+                    <div class="col-lg-6">
+                        <div align="center"><img class="img-responsive" src="images/information2.jpg" width="50%" alt="Koneksi internet lambat.."></div>
+                    </div>
+                </div><br>
+                <div class="row">
+                    <div class="col-lg-6">
+                        <div align="center"><img class="img-responsive" src="images/information3.jpg" width="50%" alt="Koneksi internet lambat.."></div>
+                    </div>
+                    <div class="col-lg-6">
+                        <div align="center"><img class="img-responsive" src="images/information4.jpg" width="50%" alt="Koneksi internet lambat.."></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div><br><br><br>
+<?php include "views/footer.php"; ?> 
